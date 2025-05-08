d240d8ffdcf3abb17b3e61e165b6e3443df1b213
Jeremy <23155614+jsbrn@users.noreply.github.com>
2025-05-08 16:37:29 -0400

    Create m3u files from spotify playlists

---
+++ scripts/m3ugen.py
@@ -0,0 +1,39 @@
+# Written for Python 3
+import time
+import os
+import subprocess
+import sys
+import argparse
+import pathlib
+import re
+import json
+import glob
+import urllib.parse
+
+SEPARATOR = "----------------------------------"
+
+# Parse command line arguments
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-p", "--playlists_file", type=pathlib.Path, required=True)
+argparser.add_argument("-l", "--library_file", type=pathlib.Path, required=True)
+argparser.add_argument("-o", "--output_folder", type=pathlib.Path, required=True)
+args = argparser.parse_args()
+
+output_folder = os.path.join(args.output_folder)
+
+with open(args.playlists_file, 'r') as file:
+    data = json.load(file)
+    for playlist in data["playlists"]:
+        with open(os.path.join(output_folder, urllib.parse.quote(str(playlist["name"])+".m3u", safe=' ')), "w") as m3u_file:
+            #playlists.append()
+            for item in playlist["items"]:
+                artist_name = item["track"]["artistName"]
+                track_name = item["track"]["trackName"]
+                path = os.path.join(output_folder, artist_name, "**/*"+str(track_name)+"*")
+                print(str(path))
+                path_matches = glob.glob(path + "", recursive=True)
+                if len(path_matches) > 0:
+                    m3u_file.write(path_matches[0]+"\n")
+                else:
+                    m3u_file.write("#"+str(track_name)+" - "+str(artist_name)+"\n")
+    file.close()
--- scripts/ripper.py
+++ scripts/ripper.py
@@ -5,9 +5,11 @@ import os
 import subprocess
 import sys
 import argparse
+import pathlib
 import re
 import json
 import glob
+from difflib import SequenceMatcher
 
 import yt_dlp
 import requests
@@ -17,10 +19,11 @@ SEPARATOR = "----------------------------------"
 
 # Parse command line arguments
 argparser = argparse.ArgumentParser()
-argparser.add_argument("-p", "--playlists_file", required=True)
-argparser.add_argument("-l", "--library_file", required=True)
-argparser.add_argument("-o", "--output_folder", required=True)
+argparser.add_argument("-p", "--playlists_file", type=pathlib.Path, required=True)
+argparser.add_argument("-l", "--library_file", type=pathlib.Path, required=True)
+argparser.add_argument("-o", "--output_folder", type=pathlib.Path, required=True)
 argparser.add_argument("-a", "--artist", default="")
+argparser.add_argument("-d", "--dryrun", type=int, default=0)
 args = argparser.parse_args()
 
 # Parse both json files into a set of of "Album :::: Artist" entries separated by newline
@@ -62,7 +65,7 @@ for album in albums:
         
         # Search Bandcamp for the album
         print("RESULTS FOR "+title+" BY "+artist+" ("+str(counter)+"/"+str(album_count)+"):")
-        search_soup = BeautifulSoup(requests.get("https://bandcamp.com/search?q="+title+"+"+artist).content, "html.parser")
+        search_soup = BeautifulSoup(requests.get("https://bandcamp.com/search?q="+title).content, "html.parser")
         results = search_soup.find_all("li", class_="searchresult")
 
         if len(results) == 0:
@@ -85,7 +88,11 @@ for album in albums:
 
             #print(clean_title, clean_entry_name, clean_artist, clean_entry_artist)
 
-            is_match = (clean_entry_name == clean_title or clean_entry_name.startswith(clean_title)) and (clean_entry_artist == clean_artist or clean_entry_artist.startswith(clean_artist))
+            artist_similar = SequenceMatcher(None, entry_artist, artist).ratio() >= .5
+            album_similar = SequenceMatcher(None, entry_name, title).ratio() >= .8
+
+            #is_match = (clean_entry_name == clean_title or clean_entry_name.startswith(clean_title)) and (clean_entry_artist == clean_artist or clean_entry_artist.startswith(clean_artist))
+            is_match = artist_similar and album_similar
             is_album = "/album" in url
             if is_match:
                 all_matches.append(url)
@@ -94,71 +101,72 @@ for album in albums:
             print("   " + ("[MATCH!] " if is_match else "         ") +  entry_name + " - "+entry_artist + "    " + url)
 
         # Pick the best match, check if it's not already downloaded, then send to yt-dlp for download
-        if len(all_matches) > 0:
-            url = all_matches[0]
-            if len(album_matches) > 0:
-                url = album_matches[0]
-            print("SELECTED URL "+url+" FOR DOWNLOAD")
+        if args.dryrun <= 0:
+            if len(all_matches) > 0:
+                url = all_matches[0]
+                if len(album_matches) > 0:
+                    url = album_matches[0]
+                print("SELECTED URL "+url+" FOR DOWNLOAD")
 
-            #Make folder if it doesn't exist
-            path = os.path.join(args.output_folder, artist, title)
-            try:
-                os.makedirs(path)
-            except OSError as error:
-                print("FAILED TO MAKE DIRECTORY "+path, error)
+                #Make folder if it doesn't exist
+                path = os.path.join(args.output_folder, artist, title)
+                try:
+                    os.makedirs(path)
+                except OSError as error:
+                    print("FAILED TO MAKE DIRECTORY "+path, error)
 
-            # Fetch album length and other details
-            existing_files_count = len(os.listdir(path))
-            page_soup = BeautifulSoup(requests.get(url).content, "html.parser")
-            track_count = max(1, len(page_soup.find_all("tr", class_="track_row_view")))
-
-            # Download if missing tracks, otherwise skip
-            print("TRACK COUNT: "+str(existing_files_count)+" / " + str(track_count))
-            if existing_files_count < track_count:
-                print("MISSING "+str(track_count-existing_files_count)+" OUT OF "+str(track_count)+" TRACKS")
-                print("DOWNLOADING...")
-
-                # Download the album
-                ytdl_opts = {
-                    "ignoreerrors": True, 
-                    "verbose": False, 
-                    "outtmpl": path+"/%(playlist_index)s - %(title)s.%(ext)s", 
-                    "writethumbnail": True,
-                    'postprocessors': [
-                        {
-                            'key': 'FFmpegMetadata'
-                        }, 
-                        {
-                            'key': 'EmbedThumbnail',
-                            'already_have_thumbnail': True, 
-                        }
-                    ] 
-                }
-                with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
-                    try:
-                        error_code = ydl.download(url)
-                    except:
-                        pass
-
-                # Clean up artwork
-                for f in glob.glob(os.path.join(path, "*.jpg")):
-                    os.remove(f)
-
-                # Verify download
+                # Fetch album length and other details
                 existing_files_count = len(os.listdir(path))
+                page_soup = BeautifulSoup(requests.get(url).content, "html.parser")
+                track_count = max(1, len(page_soup.find_all("tr", class_="track_row_view")))
+
+                # Download if missing tracks, otherwise skip
+                print("TRACK COUNT: "+str(existing_files_count)+" / " + str(track_count))
                 if existing_files_count < track_count:
-                    print("SAVED "+str(existing_files_count)+" TRACKS TO "+path)
-                    print("NOT ALL TRACKS COULD BE DOWNLOADED (LIKELY BECAUSE PREVIEWS ARE DISABLED. BUY THE ALBUM!)")
-                    print("STILL MISSING "+str(track_count-existing_files_count)+" OUT OF "+str(track_count)+" TRACKS")
-                    partially_downloaded_urls.append(title+" BY "+artist+" ("+str(existing_files_count)+"/"+str(track_count)+" DOWNLOADED)    "+url)
+                    print("MISSING "+str(track_count-existing_files_count)+" OUT OF "+str(track_count)+" TRACKS")
+                    print("DOWNLOADING...")
+
+                    # Download the album
+                    ytdl_opts = {
+                        "ignoreerrors": True, 
+                        "verbose": False, 
+                        "outtmpl": path+"/%(playlist_index)s - %(title)s.%(ext)s", 
+                        "writethumbnail": True,
+                        'postprocessors': [
+                            {
+                                'key': 'FFmpegMetadata'
+                            }, 
+                            {
+                                'key': 'EmbedThumbnail',
+                                'already_have_thumbnail': True, 
+                            }
+                        ] 
+                    }
+                    with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
+                        try:
+                            error_code = ydl.download(url)
+                        except:
+                            pass
+
+                    # Clean up artwork
+                    for f in glob.glob(os.path.join(path, "*.jpg")):
+                        os.remove(f)
+
+                    # Verify download
+                    existing_files_count = len(os.listdir(path))
+                    if existing_files_count < track_count:
+                        print("SAVED "+str(existing_files_count)+" TRACKS TO "+path)
+                        print("NOT ALL TRACKS COULD BE DOWNLOADED (LIKELY BECAUSE PREVIEWS ARE DISABLED. BUY THE ALBUM!)")
+                        print("STILL MISSING "+str(track_count-existing_files_count)+" OUT OF "+str(track_count)+" TRACKS")
+                        partially_downloaded_urls.append(title+" BY "+artist+" ("+str(existing_files_count)+"/"+str(track_count)+" DOWNLOADED)    "+url)
+                    else:
+                        print("DOWNLOAD COMPLETED SUCCESSFULLY!")
+                        successful_count = successful_count + 1
                 else:
-                    print("DOWNLOAD COMPLETED SUCCESSFULLY!")
-                    successful_count = successful_count + 1
+                    print("ALBUM ALREADY DOWNLOADED")
+                    existing_count = existing_count + 1
             else:
-                print("ALBUM ALREADY DOWNLOADED")
-                existing_count = existing_count + 1
-        else:
-            albums_not_found.append(title+" BY "+artist+" ("+str(len(results))+" results, "+str(len(all_matches))+" matches)")
+                albums_not_found.append(title+" BY "+artist+" ("+str(len(results))+" results, "+str(len(all_matches))+" matches)")
 
         # Wait a second to avoid getting rate-limited (haven't had any trouble with a 1-second delay)
         time.sleep(1)
